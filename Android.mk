4ee814913025ed416759d508ee8870af7a12f51c
Giovanni Ricca <giovanniricca@proton.me>
2025-06-12 21:47:49 +0200

    venus: Initial device-specific proprietary files list
    Change-Id: Ie7fd957cee3443654cb651331a5f1d81d5bd10d1

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),venus)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,d09d534077d3c4b5b9f9d6e8adf53939d68f2903)
+$(call add-radio-file-sha1-checked,radio/aop.img,bab2cb5fd414a80306cd819b0502bdbae7958b36)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,34aeb1dfe72df4b1b0fe4080aaedd6f7b0dfc22b)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,05741b42545ae7de3ed24da94ba6c93a8acf5a2b)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,eb82f299c2a7f0b0d585eab6a03480cfcf5718aa)
+$(call add-radio-file-sha1-checked,radio/dsp.img,453413b40e59bfbd20bcb03dce4e3538f0f04550)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,73da9cdc1e1390380a6c28cdfdf59531c1eb9d10)
+$(call add-radio-file-sha1-checked,radio/hyp.img,10cb5e1e9408a09121178cd85ae9159ff6a2985b)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,fdbdb258926f06ff5a105c02c67fa78db04e1884)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,973e7721e86b880b2a74e543b9ebae8c6a3171eb)
+$(call add-radio-file-sha1-checked,radio/modem.img,6b0b8246cceed9833e9b053fb448169c52059a04)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,22b0e199b821b8ac94e5b37f987ae9af315a1d86)
+$(call add-radio-file-sha1-checked,radio/shrm.img,09ed8fec0311ed8bb754b744396fb36955fb7f64)
+$(call add-radio-file-sha1-checked,radio/tz.img,1ede220e05f28a234a5e2c7be4ac7ea5cb1f99a2)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,261a678735b84c1abdce8230688b219181f7663d)
+$(call add-radio-file-sha1-checked,radio/xbl.img,04cc7efa03f434e63a84deb2bfb33debf2a71e6c)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,cd0ab73f5fc0052c30cd7c480216f62fb5878dc7)
+
+endif
